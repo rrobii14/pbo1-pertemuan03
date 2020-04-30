c67b430a4e40cedb025c4f46548a14c9ce975aec
rrobii14 <robi.crack1@gmail.com>
2020-04-30 21:59:00 +0800

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variabel
+
+    String nama = "Robi";
+    int jumlahSks = 24;
+    double indeksPresentasiKumulatif = 3.99;
+
+}
