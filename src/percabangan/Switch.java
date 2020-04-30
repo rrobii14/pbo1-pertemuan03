2ebb9315c4d97c8ab640c707d4c219f8fccf1d69
rrobii14 <robi.crack1@gmail.com>
2020-04-30 23:19:39 +0800

    Percabangan Switch

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=0;
+        switch(jalanJalan) {
+            case 1:
+                System.out.println("Ngabuburit");
+                break;
+            case 2:
+                System.out.println("Gabut");
+                break;
+            case 3:
+                System.out.println("Kerja Kelompok");
+                break;
+            case 4:
+                System.out.println("Auh Gelap");
+                break;
+            default:
+                System.out.println("Ga Ada Akhlak :)");
+                break;
+        }
+    }
+}
